4da055b581a3eb0cb5ab78edc346cf495b8522e9
Marc Hanheide <marc@hanheide.net>
2023-03-10 11:06:21 +0000

    turned into pure python pkg and made colcon work

--- setup.py
+++ setup.py
@@ -1,24 +1,33 @@
-#!/usr/bin/env python
-
-#  Raymond Kirk (Tunstill) Copyright (c) 2019
-#  Email: ray.tunstill@gmail.com
-
+from setuptools import setup
 import os
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
 
-
+package_name = 'topic_store'
 def parse_package_requirements():
     requirements_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
     with open(requirements_file, "r") as fh:
         return [x.strip() for x in fh.readlines()]
 
 
-setup_args = generate_distutils_setup(
-    packages=['topic_store'],
+setup(
+    name=package_name,
+    version='0.1.9',
+    packages=[package_name],
     package_dir={'': 'src'},
-    install_requires=parse_package_requirements(),
-    test_requires=["pytest"],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'].extend(parse_package_requirements()),
+    zip_safe=True,
+    maintainer='kasm-user',
+    maintainer_email='marc@hanheide.net',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'ts_convert = topic_store.convert:_convert_cli'
+        ],
+    },
 )
-
-setup(**setup_args)
